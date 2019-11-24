41a478940756e20149bc2d224e83be7ac6118f79
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2019-11-24 19:39:52 +0100

    Added two more components

---
+++ src/components/list-extra.js
@@ -0,0 +1,3 @@
+export const createListExtraTemplate = (title) => `<section class="films-list--extra">
+<h2 class="films-list__title">${title}</h2>
+</section>`;
--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@ import {createProfileTemplate} from './components/profile-name.js';
 import {createNavigationTemplate} from './components/menu.js';
 import {createSortingTemplate} from './components/sorting.js';
 import {createFilmsSectionTemplate} from './components/list.js';
+import {createFilmsContainerTemplate} from './components/films-container.js';
 import {createCardTemplate} from './components/card.js';
 import {createShowMoreButtonTemplate} from './components/button-show-more.js';
+import {createListExtraTemplate} from './components/list-extra.js';
 import {createDetailCardTemplate} from './components/popup.js';
 
 const TOTAL_CARDS = 5;
+const TOTAL_EXTRA_CARDS = 2;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
+};
+const multipleRender = (iteration, container, template) => {
+  new Array(iteration).fill(``).forEach(() => {
+    render(container, template);
+  });
 };
 const siteBodeElement = document.querySelector(`body`);
 const siteHeaderElement = document.querySelector(`.header`);
@@ -20,10 +28,23 @@ render(siteMainElement, createNavigationTemplate());
 render(siteMainElement, createSortingTemplate());
 render(siteMainElement, createFilmsSectionTemplate());
 
-const filmsListElement = document.querySelector(`.films-list`);
+const filmsMainElement = document.querySelector(`.films`);
+const filmsListElement = filmsMainElement.querySelector(`.films-list`);
+
+render(filmsListElement, createFilmsContainerTemplate());
+render(filmsMainElement, createListExtraTemplate(`Top rated`));
+render(filmsMainElement, createListExtraTemplate(`Most commented`));
+
 const filmsListEContainer = filmsListElement.querySelector(`.films-list__container`);
-new Array(TOTAL_CARDS).fill(``).forEach(() => {
-  render(filmsListEContainer, createCardTemplate());
+
+multipleRender(TOTAL_CARDS, filmsListEContainer, createCardTemplate());
+
+const filmsListExtra = filmsMainElement.querySelectorAll(`.films-list--extra`);
+
+filmsListExtra.forEach((value) => {
+  render(value, createFilmsContainerTemplate());
+  const filmsListExtraContainer = value.querySelector(`.films-list__container`);
+  multipleRender(TOTAL_EXTRA_CARDS, filmsListExtraContainer, createCardTemplate());
 });
 
 render(filmsListElement, createShowMoreButtonTemplate());
