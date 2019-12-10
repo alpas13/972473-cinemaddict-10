6646af1386234f99934f015b00072f52b65298e2
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2019-12-10 23:12:46 +0100

    Fixed remarks from last PR

--- src/components/card.js
+++ src/components/card.js
@@ -2,23 +2,30 @@ import {createElement} from "../utils";
 
 export default class FilmCard {
   constructor(card) {
-    this._card = card;
+    const {poster, title: {name, rating}, year, duration, genre, description, comments} = card;
+    this._poster = poster;
+    this._name = name;
+    this._rating = rating;
+    this._year = year;
+    this._duration = duration;
+    this._genre = genre;
+    this._description = description;
+    this._comments = comments;
     this._element = null;
   }
 
   getTemplate() {
-    const {poster, title: {name, rating}, year, duration, genre, description, comments} = this._card;
     return (`<article class="film-card">
-          <h3 class="film-card__title">${name}</h3>
-          <p class="film-card__rating">${rating}</p>
+          <h3 class="film-card__title">${this._name}</h3>
+          <p class="film-card__rating">${this._rating}</p>
           <p class="film-card__info">
-            <span class="film-card__year">${year}</span>
-            <span class="film-card__duration">${duration}</span>
-            <span class="film-card__genre">${genre}</span>
+            <span class="film-card__year">${this._year}</span>
+            <span class="film-card__duration">${this._duration}</span>
+            <span class="film-card__genre">${this._genre}</span>
           </p>
-          <img src="${poster}" alt="" class="film-card__poster">
-          <p class="film-card__description">${description}</p>
-          <a class="film-card__comments">${comments} comments</a>
+          <img src="${this._poster}" alt="" class="film-card__poster">
+          <p class="film-card__description">${this._description}</p>
+          <a class="film-card__comments">${this._comments} comments</a>
           <form class="film-card__controls">
             <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
             <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
